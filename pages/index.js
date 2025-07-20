7cef094ac88eb4e6c598540dbaaa886f6ecf5360
Matheus Santos <69777235+msbdacosta@users.noreply.github.com>
2025-07-20 13:45:53 +0000

    Home page updated to In construction

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 function Home() {
-  return <h1>Raquelzinha se me ama da uma risadinha</h1>;
-}
-
-function Teste() {
-  console.log();
+  return <h1>In Construction!</h1>;
 }
 
 export default Home;
